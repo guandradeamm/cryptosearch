89d86905c12e8af688daccb5736c67dbb087d53a
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2021-10-04 18:18:49 -0300

    feat: add axios for api fetching

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-      <h1>APPPPPI</h1>
+    <div className="coin-app">
+      <div className="coin-search"></div>
+      <h1 className="coin-text">Search a currency</h1>
+      <form>
+        <input type="text" placeholder="Search" className="coin-input" />
+      </form>
     </div>
   );
 }
