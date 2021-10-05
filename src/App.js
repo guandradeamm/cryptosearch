d4b01f2ddf30d7562791d5368d089bc335f0d17b
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2021-10-04 21:58:31 -0300

    feat: add data from API per coin

--- src/App.js
+++ src/App.js
@@ -41,7 +41,18 @@ function App() {
         </form>
       </div>
       {filteredCoins.map((coin) => {
-        return <Coin key={coin.id} name={coin.name} image={coin.image} />;
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
       })}
     </div>
   );
